0057f4f804ce00c6611eea6963e89dda3a217f54
Edward Lee <edwardjihunlee@gmail.com>
2018-11-16 18:35:32 -0800

    fixed trips page

--- src/js/trips.js
+++ src/js/trips.js
@@ -1,14 +1,14 @@
-var listOfImgs = ["https://dummyimage.com/200x200/44AA10/fff&text=Img+1",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+2",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+3",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+4",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+5",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+6",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+7",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+8",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+9",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+10",
-                      "https://dummyimage.com/200x200/44AA10/fff&text=Img+11"];
+var listOfImgs = ["https://dummyimage.com/200x200/FFF3DA/fff&text=Img+1",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+2",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+3",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+4",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+5",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+6",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+7",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+8",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+9",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+10",
+                      "https://dummyimage.com/200x200/FFF3DA/fff&text=Img+11"];
 var listOfDestinations = ["Japan",
                           "Canada",
                           "Tokyo, Japan",
@@ -21,33 +21,51 @@ var listOfDestinations = ["Japan",
                           "I'm going crazy, MyHead",
                           "Vancouver, Canada"];
 
+var listOfDates = ["Date1",
+                  "Date2",
+                  "Date3",
+                  "Date4",
+                  "Date5",
+                  "Date6",
+                  "Date7",
+                  "Date8",
+                  "Date9",
+                  "Date10",
+                  "Date11"];
+
 var nameOfUser = "Group 33"
 
 $(document).ready(function(){
   $('#trips_nav').addClass('active');
-  var cardDeck = $('#cardDeck');
+  var tripContainer = $('#trip_container');
 
+  // NAMING THE TRIPS WITH THE USERS NAME
   var page_title = $('#page_title');
   page_title.text(nameOfUser + " Trips");
+
+// <div class="trip_con">
+//   <div class="trip_background_con">
+//     <h2 class="trip_title"></h2>
+//     <p class="white-text trip_description"></p>
+//   </div>
+// </div>
   function populatePage(){
     for (var i = 0; i < listOfImgs.length; i++) {
-      var cardCon = $('<div class="col-sm-6"></div>');
-      var cardDiv = $('<div id="cardCon" class="card"></div>');
-      var cardBody = $('<div class="card-body"></div>');
-      var cardTitle = $('<h5 class="card-title"></h5>');
-      var cardText = $('<p class="card-text"></p>');
+      var tripCon = $('<div class="trip_con"></div>');
+      var tripBackgroundCon = $('<div class="trip_background_con"></div>');
+      var tripTitle = $('<h5 class="trip_title"></h5>');
+      var tripDescription = $('<p class="white-text trip_description"></p>');
+      tripBackgroundCon.append(tripTitle, tripDescription);
+      tripCon.append(tripBackgroundCon);
+      tripTitle.html(listOfDestinations[i]);
+      tripDescription.html(listOfDates[i]);
+      var backgroundStyle = {'background-image': 'url(' + '\"' + listOfImgs[i] + '\"'+ ')'}
+      tripCon.css(backgroundStyle);
 
-      cardTitle.text(listOfDestinations[i]);
-      cardDiv.css('background-image', 'url(' + listOfImgs[i] + ')');
-
-      cardBody.append(cardTitle, cardText);
-      cardDiv.append(cardBody);
-      cardCon.append(cardDiv);
-
-      cardDeck.append(cardCon);
-    }
+      tripContainer.append(tripCon);
 
   }
+}
 
   populatePage();
 
