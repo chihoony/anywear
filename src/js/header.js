53bf5dfb65c11080c590a903597eb2bd4cecd6db
BretG_BCIT <bgetz@my.bcit.ca>
2018-12-01 22:55:54 -0800

    Started rework of header

---
+++ src/js/header.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('#createTrip').on('cick', )
+});
+
+function navigate() {
+
+}
+
+{/* <li><a id="createTrip" href="/createtrip">+NewTrip</a></li>
+<li><a id="current" href="/current">CurrentTrip</a></li>
+<li><a id="allTrips" href="/alltrips">Trips</a></li>
+<li><a id="profile" href="/profile">Profile</a></li>
+<li><a id="signOut" href="/">SignOut</a></li>
+</ul> */}
