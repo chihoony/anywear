5c0e541702e785d5cb8c4b1e33679905d454ecbd
BretG_BCIT <bgetz@my.bcit.ca>
2018-12-03 16:42:36 -0800

    fixed Russia

--- src/js/trip.js
+++ src/js/trip.js
@@ -206,7 +206,7 @@ var countryCodes = [
   {name: 'Qatar', code: 'QA'},
   {name: 'Reunion', code: 'RE'},
   {name: 'Romania', code: 'RO'},
-  {name: 'Russian Federation', code: 'RU'},
+  {name: 'Russia', code: 'RU'},
   {name: 'RWANDA', code: 'RW'},
   {name: 'Saint Helena', code: 'SH'},
   {name: 'Saint Kitts and Nevis', code: 'KN'},
